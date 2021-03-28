0c4a0fb9781c0066ae6133a6d3cf64d54e1e0d73
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-29 00:06:10 +0600

    function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const app = express();
+
+function rootCall(req, res) {
+    res.send('Thank You!')
+}
+
+app.get('/', )
+
+app.listen(3000, () => console.log("Listening to Port 3000"))
