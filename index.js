852f7e4d674a7cef9d9f4ca850a98f3166eca726
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-29 00:47:04 +0600

    first api

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome Raufu Prezens!');
+    const fruit = {
+        product: 'apple',
+        price: 150
+    }
+    res.send(fruit);
 })
 
+app.get('/fruits/banana', (req, res) => {
+    res.send({ fruit: 'banana', quantity: 500, price: 5000 })
+})
 app.listen(3000, () => console.log('Listening to Port 3000'));
