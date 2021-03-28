e7a8d61a4b0ddf1aca5f33062efd19b37be6a7bf
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-29 01:05:02 +0600

    dynamic data loaded

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const users = ["Raufu", "Prezens", "Eftykhar"]
+
 app.get('/', (req, res) => {
     const fruit = {
         product: 'apple',
@@ -11,5 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/fruits/banana', (req, res) => {
     res.send({ fruit: 'banana', quantity: 500, price: 5000 })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = (req.params.id);
+    console.log(req.query.sort);
+    const name = users[id]
+    res.send({ id, name });
 })
 app.listen(3000, () => console.log('Listening to Port 3000'));
